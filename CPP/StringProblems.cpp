15245a626790db911cf05d31a713ece0fd333365
tycrowe <me@tycrowe.com>
2020-01-05 17:56:36 -0500

    Changing naming schemes to match C++ 14 standard. Added more problems.

--- CPP/StringProblems.cpp
+++ CPP/StringProblems.cpp
@@ -1,3 +1,7 @@
+/**
+ * @author Tyler Crowe
+ * @version v1.0.0 - January 5th, 2020
+ */
 #include <iostream>
 #include <string>
 
@@ -11,7 +15,7 @@ using namespace std;
  *  true: the string contains all unique characters.
  *  false: the string has one or more duplicate characters.
  */
-bool isUnique(string str) {
+bool is_unique(string str) {
     if (str.empty()) {
         return false;
     } else {
@@ -35,7 +39,7 @@ bool isUnique(string str) {
  *  True: Is a permutation
  *  False: Is not a permutation
  */
-bool checkPermutation(string a, string b) {
+bool check_permutation(string a, string b) {
     if ((a.empty() || b.empty()) && (a.size() != b.size())) {
         return false;
     } else {
@@ -50,14 +54,37 @@ bool checkPermutation(string a, string b) {
     }
 }
 
+/**
+ * Given a string of 'length', replace all appropriate whitespace with "%20". Appropriate whitespace is defined as a
+ * space between two words. For example, "Tyler Crowe   ". The trailing whitespace at the end of the former string is
+ * noted as "inappropriate" whitespace.
+ * Time complexity:
+ * @param a - The given input string to convert into a url.
+ * @param length - The "true" length of string. Which is determined by the size minus inappropriate whitespace capacity.
+ * @return A new string, of the given string, now represented as a URL.
+ */
+string make_url(string a, int length) {
+    string url;
+
+    for (int i = 0; i < length; ++i) {
+        if(a[i] == ' ') {
+            url.append("%20");
+        } else {
+            url.append(1, a[i]);
+        }
+    }
+
+    return url;
+}
+
 int main() {
     // True
     string unique_test_1 = "abcdefghijklmnopqrstuvwxyz";
     // False
     string unique_test_2 = "UNIQUE";
 
-    cout << isUnique(unique_test_1) << endl;
-    cout << isUnique(unique_test_2) << endl;
+    cout << is_unique(unique_test_1) << endl;
+    cout << is_unique(unique_test_2) << endl;
 
     // True
     string permutation_test_1_a = "abc";
@@ -67,8 +94,17 @@ int main() {
     string permutation_test_2_a = "mama";
     string permutation_test_2_b = "papa";
 
-    cout << checkPermutation(permutation_test_1_a, permutation_test_1_b) << endl;
-    cout << checkPermutation(permutation_test_2_a, permutation_test_2_b) << endl;
+    cout << check_permutation(permutation_test_1_a, permutation_test_1_b) << endl;
+    cout << check_permutation(permutation_test_2_a, permutation_test_2_b) << endl;
+
+    string url_test_1 = "Mr John Smith";
+    int true_length_1 = 13;
+
+    string url_test_2 = "alluring cute kitten video";
+    int true_length_2 = 26;
+
+    cout << make_url(url_test_1, true_length_1) << endl;
+    cout << make_url(url_test_2, true_length_2) << endl;
 
     return 0;
 }
