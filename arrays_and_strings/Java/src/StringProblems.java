f6a803ba158a5b40651a3c1eb93784f0fca21b68
tycrowe <me@tycrowe.com>
2020-01-07 14:29:07 -0500

    Adding more string problems and their solutions with Java implementation.

---
+++ arrays_and_strings/Java/src/StringProblems.java
@@ -0,0 +1,24 @@
+/**
+ * @author Tyler Crowe
+ * @version v1.0.0 - 1/7/2019
+ */
+public class StringProblems {
+
+    /**
+     * Method for compressing a string. For this algorithm, we're given a string as a parameter and
+     * asked to "compress" it down to it's character and count representation. For example: The
+     * string AAaaBBbbCCcc becomes A2a2B2b2C2c2. Note, these two strings are equal in length. When
+     * this occurs, we return the given string. We only return the compressed string when the length
+     * after compression is less than the given string's length.
+     * @param a - The given string to compress.
+     * @return - The compressed string.
+     */
+    private String compressString(String a) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        new StringProblems();
+    }
+
+}
